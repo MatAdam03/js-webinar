a84601b6104683896ba3a5eb4e1fb9d8d2f8e362
Ádám Matil <adam_matil@epam.com>
2021-03-07 14:31:40 +0100

    Added module-1

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -15,7 +15,18 @@ function fibonacci(n) {
      * Also take into consideration the documentation of the function!
      */
     // PLACE YOUR CODE BETWEEN THIS...
-
+    if(n>=0){
+        let arr=[0,1]
+        for (let index = 0; index < n; index++) {
+            const tmp=arr[1]
+            arr[1]=arr[0]+arr[1]
+            arr[0]=tmp           
+        }
+        nThFibonacci=arr[0]
+    }else{
+        nThFibonacci=0;
+    }
+    
     // ...AND THIS COMMENT LINE!
     return nThFibonacci;
 }
